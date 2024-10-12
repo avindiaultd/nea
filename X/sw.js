9cdd16e14092e386b6fd8936d50e7d3200683a4b
AV INDIA ULTD <168712840+avindiaultd@users.noreply.github.com>
2024-10-12 14:38:57 +0530

    Update sw.js

--- X/sw.js
+++ X/sw.js
@@ -1,11 +1,13 @@
 importScripts('https://www.gstatic.com/firebasejs/7.14.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/7.14.0/firebase-messaging.js');
-var firebaseConfig = {
-    apiKey: "AIzaSyDDFphd7ioHLcmCfXt-eta8ljX0ex2yV6s",
-    projectId: "my-project-1409b",
-    storageBucket: "com.myproject.myproject",
-    messagingSenderId: "130527465681",
-    appId: "1:130527465681:android:7ff6c5732b020575e275f0"
+const firebaseConfig = {
+  apiKey: "AIzaSyDHL4CSEjJBdNUI4IozRI-7Suqd4UqUoDI",
+  authDomain: "avindia-db2bd.firebaseapp.com",
+  databaseURL: "https://avindia-db2bd-default-rtdb.firebaseio.com",
+  projectId: "avindia-db2bd",
+  storageBucket: "avindia-db2bd.appspot.com",
+  messagingSenderId: "970872368951",
+  appId: "1:970872368951:web:fe17e10b3a366f86c6b4b2"
 };
 
 firebase.initializeApp(firebaseConfig);
